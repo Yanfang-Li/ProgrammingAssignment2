56231cc283c6f18ba0740774154a7bfd61cac8ab
Yanfang Li <liyanfang1110@gmail.com>
2016-03-03 14:52:13 -0500

    Add more comments in cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,11 @@
 ## Assignment: Caching the Inverse of a Matrix
 
 ## This function creates a special "matrix" object that can cache its inverse
+# It creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
         invx <- NULL
